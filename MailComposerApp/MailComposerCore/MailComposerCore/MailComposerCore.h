e3a415f23fd60718bdaf14667a0552f6a947cce3
SHENU GUPTA <shenu.gupta2018@gmail.com>
2021-08-03 19:33:05 +0530

    text editor core framework created

---
+++ MailComposerApp/MailComposerCore/MailComposerCore/MailComposerCore.h
@@ -0,0 +1,22 @@
+//
+//  MailComposerCore.h
+//  MailComposerCore
+//
+//  Created by SHENU GUPTA on 03/08/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MailComposerCore.
+FOUNDATION_EXPORT double MailComposerCoreVersionNumber;
+
+//! Project version string for MailComposerCore.
+FOUNDATION_EXPORT const unsigned char MailComposerCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MailComposerCore/PublicHeader.h>
+
+
+#import <MailComposerCore/PRTextStorage.h>
+#import <MailComposerCore/PREditorContentName.h>
+
+
---
+++ MailComposerApp/MailComposerFramework/MailComposerFramework/MailComposerFramework.h
@@ -0,0 +1,18 @@
+//
+//  MailComposerFramework.h
+//  MailComposerFramework
+//
+//  Created by SHENU GUPTA on 03/08/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MailComposerFramework.
+FOUNDATION_EXPORT double MailComposerFrameworkVersionNumber;
+
+//! Project version string for MailComposerFramework.
+FOUNDATION_EXPORT const unsigned char MailComposerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MailComposerFramework/PublicHeader.h>
+
+
